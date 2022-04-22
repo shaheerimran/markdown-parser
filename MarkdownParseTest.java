831bebf91a241ad2c0370c95f933a72769199b51
Shaheer <shahimr1234@gmail.com>
2022-04-21 19:06:14 -0700

    Added test for getLinks()

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,26 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void getLinks() throws IOException {
+        //Expected list to be returned
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("https://something.com");
+        expected.add("some-thing.html");
+        
+        //Getting file contents
+        Path testFile = Path.of("test-file.md");
+        String testFileContent = Files.readString(testFile);
+        assertEquals(expected, MarkdownParse.getLinks(testFileContent));
     }
 }
